eca6c513cd6ccfa99ed8c95ab2854c3d107d5e08
Min Song <min.song@yonsei.ac.kr>
2021-04-18 18:12:43 +0900

    Add files via upload

--- examples/test_term_weighting.py
+++ examples/test_term_weighting.py
@@ -17,8 +17,9 @@ print()
 
 print('==  ==')
 
+label_list = []
 documents = []
-for doc in result:
+for i, doc in enumerate(result):
     document = ' '
     for sent in doc:
         for word in sent:
@@ -28,40 +29,44 @@ for doc in result:
     if len(document) > 0:
        documents.append(document)
 
+    if i % 5 == 0:
+        label_list.append('POLITICS')
+    else:
+        label_list.append('UNI')
 #print(documents)
 
 weight_algorithm = 'tf_idf'
 
 if weight_algorithm == 'tf_idf':
-    tf_idf = ptm.weighting.TfIdf(documents)
+    tf_idf = ptm.weighting.TfIdf(documents, label_list=label_list)
     weights = tf_idf()
 
 elif weight_algorithm == 'tf_bdc':
-    tf_bdc = ptm.weighting.TfBdc(documents)
+    tf_bdc = ptm.weighting.TfBdc(documents, label_list=label_list)
     weights = tf_bdc()
 
 elif weight_algorithm == 'iqf_qf_icf':
-    iqf_qf_icf = ptm.weighting.IqfQfIcf(documents)
+    iqf_qf_icf = ptm.weighting.IqfQfIcf(documents, label_list=label_list)
     weights = iqf_qf_icf()
 
 elif weight_algorithm == 'tf_chi':
-    tf_chi = ptm.weighting.TfChi(documents)
+    tf_chi = ptm.weighting.TfChi(documents, label_list=label_list)
     weights = tf_chi()
 
 elif weight_algorithm == 'tf_dc':
-    tf_dc = ptm.weighting.TfDc(documents)
+    tf_dc = ptm.weighting.TfDc(documents, label_list=label_list)
     weights = tf_dc()
 
 elif weight_algorithm == 'tf_eccd':
-    tf_eccd = ptm.weighting.TfEccd(documents)
+    tf_eccd = ptm.weighting.TfEccd(documents, label_list=label_list)
     weights = tf_eccd()
 
 elif weight_algorithm == 'tf_ig':
-    tf_ig = ptm.weighting.TfIg(documents)
+    tf_ig = ptm.weighting.TfIg(documents, label_list=label_list)
     weights = tf_ig()
 
 elif weight_algorithm == 'tf_rf':
-    tf_rf = ptm.weighting.TfRf(documents)
+    tf_rf = ptm.weighting.TfRf(documents, label_list=label_list)
     weights = tf_rf()
 
 print(weights)
