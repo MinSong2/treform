4a1736529e47b1ff26044fe60b43ebce571337a8
Min Song <min.song@yonsei.ac.kr>
2022-05-04 09:07:09 +0900

    Add files via upload

--- examples/test_term_burstiness.py
+++ examples/test_term_burstiness.py
@@ -1,6 +1,37 @@
 import treform as ptm
 
-from treform.weighting.term_burstiness import compute_term_burstiness
+from treform.weighting.term_burstiness import compute_pure_term_burstiness
 
 dataset = '../sample_data/news_articles_201701_201812.csv'
-compute_term_burstiness(dataset)
+
+pipeline = ptm.Pipeline(ptm.splitter.NLTK(),
+                            ptm.tokenizer.Komoran(),
+                            ptm.helper.POSFilter('NN*'),
+                            ptm.helper.SelectWordOnly(),
+                            #ptm.ngram.NGramTokenizer(2, 2),
+                            ptm.helper.StopwordFilter(file='../stopwords/stopwordsKor.txt')
+                            )
+
+
+corpus = ptm.CorpusFromFieldDelimitedFileForClassification(dataset,delimiter=',',doc_index=4,class_index=0)
+result = pipeline.processCorpus(corpus.docs)
+
+#print(result)
+print()
+
+print('==  ==')
+label_list = []
+documents = []
+for i, doc in enumerate(result):
+    document = ' '
+    for sent in doc:
+        for word in sent:
+            if len(word) > 0:
+                document += ' ' + word
+    document = document.strip()
+    if len(document) > 0:
+       documents.append(document)
+    _label = corpus.pair_map[i]
+    label_list.append(_label)
+
+compute_pure_term_burstiness(documents,label_list)
